fd7e109c5160cab8690d1409e2e6f3ab75553113
Darcy Ryan <dryan@dgitsystems.com>
2020-12-18 09:34:47 +1100

    Day 17 Part 1

---
+++ 17/part1.rs
@@ -0,0 +1,81 @@
+use itertools::Itertools;
+use std::convert::TryFrom;
+
+const GRID_SIZE: usize = 20;
+const INPUT_OFFSET: usize = 6;
+const NCYCLES: usize = 6;
+
+#[derive(Default)]
+struct Grid {
+    grid: [[[bool; GRID_SIZE]; GRID_SIZE]; GRID_SIZE],
+}
+
+impl Grid {
+    fn get(&self, x: isize, y: isize, z: isize) -> bool {
+        let x = match usize::try_from(x) { Ok(a) => a, _ => return false };
+        let y = match usize::try_from(y) { Ok(a) => a, _ => return false };
+        let z = match usize::try_from(z) { Ok(a) => a, _ => return false };
+
+        self.grid.get(z)
+            .and_then(|layer| layer.get(y))
+            .and_then(|row| row.get(x))
+            .copied()
+            .unwrap_or_default()
+    }
+}
+
+fn main() {
+    let mut grid = Grid::default();
+    let mut grid_new = Grid::default();
+
+    for (y, line) in include_str!("input.txt").lines().enumerate() {
+        for (x, chr) in line.chars().enumerate() {
+            if chr == '#' {
+                grid.grid[INPUT_OFFSET][y + INPUT_OFFSET][x + INPUT_OFFSET] = true;
+            }
+        }
+    }
+
+    for _cycle in 0..NCYCLES {
+        next_grid(&grid, &mut grid_new);
+        std::mem::swap(&mut grid, &mut grid_new);
+    }
+
+    let count = grid.grid.iter()
+        .flat_map(|layer| layer.iter())
+        .flat_map(|row| row.iter())
+        .filter(|active| **active)
+        .count();
+
+    println!("{}", count);
+}
+
+fn next_grid(curr_grid: &Grid, next_grid: &mut Grid) {
+    for x in 0..GRID_SIZE {
+        for y in 0..GRID_SIZE {
+            for z in 0..GRID_SIZE {
+                let active = curr_grid.get(x as isize, y as isize, z as isize);
+                let n = neighbours(x as isize, y as isize, z as isize)
+                    .filter(|&(x, y, z)| curr_grid.get(x, y, z))
+                    .count();
+                let result = match (active, n) {
+                    (true, 2) => true,
+                    (true, 3) => true,
+                    (false, 3) => true,
+                    _ => false,
+                };
+                next_grid.grid[z][y][x] = result;
+            }
+        }
+    }
+}
+
+fn neighbours(x: isize, y: isize, z: isize)
+        -> impl Iterator<Item = (isize, isize, isize)> {
+    [-1isize, 0isize, 1isize].iter().cloned()
+        .cartesian_product([-1isize, 0isize, 1isize].iter().cloned())
+        .cartesian_product([-1isize, 0isize, 1isize].iter().cloned())
+        .map(|((x, y), z)| (x, y, z))
+        .filter(|coord| *coord != (0, 0, 0))
+        .map(move |(dx, dy, dz)| (x + dx, y + dy, z + dz))
+}
